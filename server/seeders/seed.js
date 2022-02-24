1f0c4be5b10540eb0fe5b60e2c2e7894ef1aebe7
DanielWestiner <daniel.westiner@gmail.com>
2022-02-24 18:11:35 -0500

    updated seed data for fandoms, updated to new scripts in package.json

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,24 @@
 const db = require('../config/connection');
 const { User } = require('../models');
 const profileSeeds = require('./profileSeeds.json');
+const fandomSeeds = require('./fandomSeeds.json')
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
     await User.create(profileSeeds);
+
+    console.log('all done!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
+
+db.once('open', async () => {
+  try {
+    await User.deleteMany({});
+    await User.create(fandomSeeds);
 
     console.log('all done!');
     process.exit(0);
