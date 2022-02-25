790a2d4c3db97f33b66f9912b76be6d9d10a29e4
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-02-25 12:14:06 -0500

    edits to card

---
+++ client/src/components/layout/Cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/Cards.css';
+
+function Cards() {
+  return(
+    <div className='container'>
+      <div className='row s12 right contact-links'>
+       <p>&copy; Copyright 2022 TandemFandom</p>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
