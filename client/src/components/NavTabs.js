9e0316b39ca9b6e76451c56a6111d4b3e49eee14
Jose Dominguez <jdomindev@gmail.com>
2022-02-25 11:47:53 -0500

    fixed package.json

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -36,6 +36,16 @@ function NavTabs({ currentPage, handlePageChange }) {
           Matches
         </a>
       </li>
+      <li className="nav-item">
+        <a
+          href="#login"
+          onClick={() => handlePageChange('Login')}
+          // Check to see if the currentPage is `Matches`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Login' ? 'nav-link active' : 'nav-link'}
+        >
+          Login
+        </a>
+      </li>
     </ul>
   );
 }
