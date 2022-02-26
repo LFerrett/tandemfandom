8e6f51948bfd64a5c66f2a32be3cfd506fb32cfd
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-26 14:12:20 -0500

    Styled landing page and nav

--- client/src/components/layout/NavTabs.js
+++ client/src/components/layout/NavTabs.js
@@ -7,12 +7,12 @@ import logo from "../images/TFLogo.svg";
 function NavTabs() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light py-3">
-      <a className="navbar-brand" href="#"><img src={logo} alt="Tandem Fandom"/></a>
+    <a className="navbar-brand" href="#"><img src={logo} alt="Tandem Fandom"/></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div className="collapse navbar-collapse" idName="navbarNav">
+  <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav mr-auto">
      
       <li className="nav-item">
--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import './../assets/Landing.css'
-import Logo from './../images/TFLogo.svg';
-import LandingCard from '../layout/LandingCard';
+import React from "react";
+import "./../assets/Landing.css";
+import SmoothScroll from "smooth-scroll";
+import logo from '../images/TFLogo_white.svg';
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
 
 export default function Landing() {
   return (
-    <div className='container'>
-      <img src={Logo} alt="Tandem Fandom logo" height="200" align=""></img>
-      <h1></h1>
-      <p>
-      Tandem Fandom is THE people finding app for the Who's Whovian of your favorite fandoms!
-      </p>
-      <p>LOGIN</p>
-      <p>SIGNUP</p>
-    </div>
-  );
+      <header id='header'>
+        <div className='intro'>
+          <div className='overlay'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-md-12 intro-text'>
+                <img src={logo} alt="Tandem Fandom" width="50%"/>
+                  <p>Find your people! Tandem Fandom is THE people finding app for the Who's Whovian of your favorite fandoms!</p>
+                  <a
+                    href='/Signup'
+                    className='btn btn-custom btn-lg'
+                  >
+                    Sign Up
+                  </a>{' '}
+                  <a
+                    href='/Login'
+                    className='btn btn-custom btn-lg'
+                  >
+                    Login
+                  </a>{' '}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    )
 }
