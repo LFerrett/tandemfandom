c48013fd54d52d9431fe93ff5aa4bedb2f5eb266
Jose Dominguez <jdomindev@gmail.com>
2022-02-28 17:09:06 -0500

    functionality working on matches page

--- client/src/components/MatchesList.js
+++ client/src/components/MatchesList.js
@@ -1,19 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
 import { useMutation } from "@apollo/client";
-import Auth from '../utils/auth'
+import Auth from "../utils/auth";
 
 import { ADD_MATCH } from "../utils/mutations";
 
 export default function MatchesList({ users, me }) {
-  console.log({users}, {me})
-  // const [savedUserIds, setSavedUserIds] = useState(getSavedUserIds());
-  const [userId, setUserId] = useState();
-  
-  const [addMatch, {error}] = useMutation(ADD_MATCH)
-  
-  
-  const handleClick = async (matchId) => {
+  console.log({ users }, { me });
+  const [unMatches, setUnMatches] = useState([]);
+  const [matches, setMatches] = useState(me.matches);
+  // const [matches, setMatches] = useState(me.matches);
 
+  const [addMatch, { error }] = useMutation(ADD_MATCH);
+
+  const handleClick = async (matchId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -25,46 +25,129 @@ export default function MatchesList({ users, me }) {
         variables: { _id: matchId },
       });
 
+      setUnMatches([...unMatches.filter((match) => match._id !== matchId)]);
+
       Auth.login(data.users.token);
     } catch (err) {
       console.error(JSON.parse(JSON.stringify(err)));
     }
   };
 
-  const unmatchedUsers = users.filter(user => me.matches.map((match, index) => user._id !== match._id ))
-  console.log(unmatchedUsers)
+  useEffect(() => {
+    loadMatches();
+  }, []);
+  // const unmatchedUsers = users.filter(user => me.matches.map((match, index) => user._id !== match._id ))
+  // console.log(unmatchedUsers)
+  function loadMatches() {
+    const notMeUsers = users.filter((user) => user._id !== me._id);
+    console.log(notMeUsers);
+    const meMatches = me.matches.map((match) => match._id);
+    console.log(meMatches);
+    // let unmatchedUsers;
+    const unmatchedUsers = notMeUsers.filter((user, index) => {
+      console.log(meMatches[index]);
+      if (meMatches[index] === undefined) {
+        return true;
+      } else {
+        return false;
+      }
+
+      // const banana = me.matches.map((match) => {
+      //   console.log(index);
+      // });
+      // console.log(banana);
+    });
+    setUnMatches(unmatchedUsers);
+  }
+  // for (let index = 0; index < meMatches.length; index++) {
+  //   const matchId = meMatches[index];
+  //   if (matchId) {
+
+  //   }
+  // }
+  // console.log(unmatchedUsers);
+
+  if (!Auth.loggedIn()) {
+    return <Redirect to="/login" />
+  };
 
   return (
     <div>
       <h1>Matches</h1>
-      <div className="row" >
-      
-      {users.map((user, index) => {
-        return (
-          <div className="col my-4" key={user._id}>
-          <div className="card" style={{ width: `18rem` }}>
-            <img
-              className="card-img-top"
-              src={`${user.image}`}
-              alt="users profile"
-            />
-            <div className="card-body">
-              <h5 className="card-title text-center">{user.firstName} {user.lastName}</h5>
-              <h6 className="card-text">Fandoms:</h6>
-              {user.fandoms.map((fandom) => {
-                return (
-                  <p className="card-text" key={fandom._id}>{fandom.name}</p>
-                );
-              })}
-              <div className="text-center">
-                <button className='btn-block btn-success' type="button" onClick={() => handleClick(user._id)}>Add Match</button>
+      <div className="row">
+        {matches.map((user, index) => {
+          return (
+            <div className="col my-4" key={user._id}>
+              <div className="card" style={{ width: `18rem` }}>
+                <img
+                  className="card-img-top"
+                  src={`${user.image}`}
+                  alt="users profile"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-center">
+                    {user.firstName} {user.lastName}
+                  </h5>
+                  <h6 className="card-text">Fandoms:</h6>
+                  {/* {user.matches.fandoms.map((fandom) => {
+                    return (
+                      <p className="card-text" key={fandom._id}>
+                        {fandom.name}
+                      </p>
+                    );
+                  })} */}
+                  <div className="text-center">
+                    <button
+                      className="btn-block btn-success"
+                      type="button"
+                      onClick={() => handleClick(user._id)}
+                    >
+                      Add Match
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          </div>
-          
-        );
-      })}
+          );
+        })}
+      </div>
+      <h1>UnMatches</h1>
+      <div className="row">
+        {unMatches.map((user, index) => {
+          return (
+            <div className="col my-4" key={user._id}>
+              <div className="card" style={{ width: `18rem` }}>
+                <img
+                  className="card-img-top"
+                  src={`${user.image}`}
+                  alt="users profile"
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-center">
+                    {user.firstName} {user.lastName}
+                  </h5>
+                  <h6 className="card-text">Fandoms:</h6>
+                  {user.fandoms.map((fandom) => {
+                    return (
+                      <p className="card-text" key={fandom._id}>
+                        {fandom.name}
+                      </p>
+                    );
+                  })}
+                  <div className="text-center">
+                    <button
+                      className="btn-block btn-success"
+                      type="button"
+                      onClick={() => handleClick(user._id)}
+                    >
+                      Add Match
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -9,6 +9,20 @@ db.once('open', async () => {
     await Fandom.deleteMany({});
     await User.create(profileSeeds);
     await Fandom.create(fandomSeeds);
+    const allFandoms = await Fandom.find({})
+    const allUsers = await Users.find({})
+
+
+    // const fandomIds = allFandoms.map(fandom => fandom._id)
+    // console.log(fandomIds)
+    // for (let index = 0; index < allUsers.length; index++) {
+    //   const singleUser = allUsers[index];
+    //   singleUser.fandoms.push(fandomIds[Math.floor(Math.random() * fandomIds.length)])
+    //   console.log(singleUser)
+    // }
+    // math.random on fandomIds[random]
+
+  
 
     console.log('all done!');
     process.exit(0);
