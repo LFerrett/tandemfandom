5af40146319850a6d76afca9b31cfcb176becf94
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-28 13:33:50 -0500

    Added logout functionality start... not done yet

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -35,7 +35,8 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/Profile');
+    // window.location.assign('/Profile');
+    window.location.replace('/Profile');
   }
 
   logout() {
